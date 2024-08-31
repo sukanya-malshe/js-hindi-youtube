972346562385588d50de25c46942f956bb56e6b7
sukanya-malshe <sukanyamalshe1@gmail.com>
2024-08-31 19:09:36 +0000

    declared variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,31 @@
+const accountId =8005465065;   // memory = memoryname = assigned value
+let   accountEmail="manasi@wipro.com";
+var   accountPassword ="Rudransh@2023";
+      accountCity = "Dapoli";
+let   accountState;
+
+//investication start------
+
+// accountId=80045789;         //const keywords not change.
+ {
+    // ye curly brackets ko scope boalte hai.
+ }
+
+//  let   accountState; variable declared karke chod deto hai wo undfined hota hai. not defined.
+
+ /*
+    Prefer not to use var
+    because of issue in block scope and functional scope.
+ */
+
+
+
+ accountEmail = "ravi@facebook.com";
+ accountPassword = "radha@4532";
+ accountCity = "Mumbai";
+
+ console.log(accountId);
+
+ console.table([accountEmail,accountPassword,accountCity,accountState]);
+
+
